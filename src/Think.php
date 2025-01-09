872458ef10d4db3086dc060824b615c79fec5030
thinkphp <thinkphp@qq.com>
2025-01-09 10:46:04 +0800

    改进

--- src/Think.php
+++ src/Think.php
@@ -180,6 +180,7 @@ class Think implements TemplateHandlerInterface
             $path = $this->config['view_path'];
         } else {
             $path = $this->getViewPath($app ?? $this->app->http->getName());
+            $this->template->view_path = $path;
         }
 
         $depr = $this->config['view_depr'];
